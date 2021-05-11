bfbba111b335d3da12b0c8aee399bed1c39cb576
Evgeniy Savkov <EvgeniySavkov@itcenterifmo.onmicrosoft.com>
2021-05-11 06:17:01 +0300

    Add project files.

---
+++ WhatDay1/WhatDay.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WhatDay1
+{
+    enum MonthName
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+
+    class WhatDay
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a day number between 1 and 365: ");
+            string line = Console.ReadLine();
+            int dayNum = int.Parse(line);
+            int monthNum = 0;
+
+
+            var DaysInMonths = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            foreach (int daysInMonth in DaysInMonths)
+            {
+                if (dayNum <= daysInMonth)
+                {
+                    break;
+                }
+                else
+                {
+                    dayNum -= daysInMonth;
+                    monthNum++;
+                }
+            }
+            MonthName temp = (MonthName)monthNum;
+            string monthName = temp.ToString();
+
+            Console.WriteLine("{0} {1}", dayNum, monthName);
+            Console.ReadKey();
+        }
+
+
+    }
+    // Don't modify anything below here
+    //static System.Collections.ICollection DaysInMonths
+    //        = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+    }
+
